21467eaefd2551b4d2e72ee4427813257f5eccc5
paggoo <paggo@hotmail.de>
2024-08-15 10:59:42 +0200

    generalized part_paint

--- src/part_paint.py
+++ src/part_paint.py
@@ -23,7 +23,7 @@ def color_nth_advanced_face(file, entry_number, color):
     lines = extract_lines(file)
     af = find_all_advanced_faces_entries(lines)[entry_number]
     last = get_highest_hash_entry(lines)
-    add_override_to_MDGPR(file, last+1);
+    add_override_to_MDGPR(file, last+1)
     insert_hash_entry(file, "#" + str(last+1) + " = OVER_RIDING_STYLED_ITEM('overriding color',(#" + str(last+2) + "),#" + str(af) + ",'');")
     insert_hash_entry(file, "#" + str(last+2) + " = PRESENTATION_STYLE_ASSIGNMENT((#" + str(last+3) + "));")
     insert_hash_entry(file, "#" + str(last+3) + " = SURFACE_STYLE_USAGE(.BOTH.,#" + str(last+4) + ");")
@@ -56,7 +56,7 @@ def color_multiple_faces(file):
 
 
 def toggle_faces_color(path):
-    # manual differenciation if this face belongs to the classified class (fastener) or not
+    # manual differenciation whether this face belongs to the classified class (fastener) or not
     selected = "red"
     uncolored = "yellow"
     colored = "blue"
@@ -65,7 +65,7 @@ def toggle_faces_color(path):
     num_faces = len(find_all_advanced_faces_entries(lines))
     # all uncolored first
     for i in range(num_faces):
-        color_nth_advanced_face("../data/Control_levers_n.stp", i, uncolored)
+        color_nth_advanced_face(path, i, uncolored)
     #print("no face colored")
     #view_stp(full_path)
     print("select surfaces belonging to desired class. " + colored + "=in, " + uncolored + "=out.\n")
@@ -75,18 +75,18 @@ def toggle_faces_color(path):
                 if key.char == 'n':
                     # deselect current face
                     print("n pressed. deselecting surface " + str(i))
-                    color_nth_advanced_face("../data/Control_levers_n.stp", i, uncolored)
+                    color_nth_advanced_face(path, i, uncolored)
                     # Stop listener
                     return False
                 if key.char == 'y':
                     # select current face
-                    print("y pressed. selecting surnface " + str(i))
-                    color_nth_advanced_face("../data/Control_levers_n.stp", i, colored)
+                    print("y pressed. selecting surface " + str(i))
+                    color_nth_advanced_face(path, i, colored)
                     # Stop listener
                     return False
             except:
                 pass  # special key pressed
-        color_nth_advanced_face("../data/Control_levers_n.stp", i, selected)
+        color_nth_advanced_face(path, i, selected)
         viewer_process = view_stp(full_path)
         sleep(1)
         print("does " + selected + " surface belong to the desired class?: press y or n \n")
@@ -95,13 +95,14 @@ def toggle_faces_color(path):
     viewer_process = view_stp(full_path)
     sleep(1)
     print("(optional) final result. you can close viewer or press enter to close it.")
-    def on_release(key):
-        if key == Key.enter:
-            # deselect current face
-            print("Enter pressed. closing viewer ")
-            close_stp_viewer(viewer_process)
-            # Stop listener
-            return False
+
+    # def on_release(key):
+    #     if key == Key.enter:
+    #         # deselect current face
+    #         print("Enter pressed. closing viewer ")
+    #         close_stp_viewer(viewer_process)
+    #         # Stop listener
+    #         return False
 
 
 #baugruppe bestehend aus mehreren shapes
@@ -109,8 +110,9 @@ def toggle_faces_color(path):
 #was indiziert NEXT_ASSEMBLY_USAGE_OCCURRENCE
 #PRODUCT_DEFINITION_FORMATION_WITH_SPECIFIED_SOURCE
 p = "../data/Wheelbarrow.stp"
+p = "../data/Control_levers_n.stp"
 #print(get_highest_hash_entry(extract_lines(p)))
-#toggle_faces_color(p)
+toggle_faces_color(p)
 
 
 # zuerst mal "Innenzylinder" färben um so Löcher für Schrauben und Nieten zu ermitteln?
